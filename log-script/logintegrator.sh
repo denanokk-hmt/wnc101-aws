9800e8da5a05e49cec8b4ad614314b7a42ad23c5
ta hi <denanokk@gmail.com>
2018-06-27 11:44:22 +0900

    ログ統合スクリプト更新中

--- log-script/logintegrator.sh
+++ log-script/logintegrator.sh
@@ -4,8 +4,8 @@
 # Logrotate & S3 uploader
 #
 # 引数
-# $1:logファイルのDirPath
-# $2:統合する期間 YYYYMMDD-YYYYMMDD 
+# $1:統合する期間 YYYYMMDD-YYYYMMDD 
+# $2:顧客(例：Urban researc->ur)
 #
 # Written by hiramatsu 2018.6.25
 ########################################
@@ -31,48 +31,78 @@ ERR_CHK() {
 #基本設定
 ################################
 cd `dirname $0`
-tmp=/tmp/tmp_$1
 LANG=ja_JP.UTF-8
 SYSD=$(pwd)
 ENV=${SYSD}/ENV/env
 S3_DL=${SYSD}/S3_DL
-
-
-################################
-#VALIDATION1
-################################
-if [ -z "$1" ]; then
-  echo "[VAILD ERR] You do not set 1st option dir path."
-  exit 0
-fi
-if [ -z "$2" ]; then
-  echo "[VALID ERR] You do not set 2nd option FROM-TO."
-  exit 0
-fi
+tmp=${S3_DL}/tmp
 
 
 ################################
 #引数
 ################################
-LOGD=$1
+CUST=$1
 FROM_TO=$2
 FROM=${FROM_TO:0:8}
 TO=${FROM_TO:9:8}
 
+
 ################################
-#VALIDATION2
+#VALIDATION FROM-TO
 ################################
-if [ -e "$1" ]; then
+if [ -z "$1" ]; then
+  echo "[VALID ERR] dose not set 1st arg FROM-TO."
+  exit 0
+fi
+expr "${FROM} + 1" > /dev/null 2>&1
+if [ $? -lt 2 ] ; then
   :
 else
-  echo "[VAILD ERR] Your set dir dose not exits."
+  echo "FROM of FROM-TO dose not number."
+  exit 0
+fi
+expr "${TO} + 1" > /dev/null 2>&1
+if [ $? -lt 2 ] ; then
+  :
+else
+  echo "TO of FROM-TO dose not number."
+  exit 0
+fi
+if [ "${#FROM}" -eq 8 ]; then
+  :
+else
+  echo "FROM of FROM-TO dose not 8 char. YYYYMMDD."
+  exit 0
+fi
+if [ "${#TO}" -eq 8 ]; then
+  :
+else
+  echo "TO of FROM-TO dose not 8 char. YYYYMMDD."
+  exit 0
+fi
+#if [ "`date +'%Y%m%d' -d ${FROM} 2> /dev/null`" == ${FROM} ]; then
+#  :
+#else
+#  echo "FROM of FROM-TO is ng."
+#  exit 0
+#fi
+#if [ "`date +'%Y%m%d' -d ${TO} 2> /dev/null`" == ${TO} ]; then
+#  :
+#else
+#  echo "TO of FROM-TO is ng."
+#  exit 0
+#fi
+if [ ${FROM} -gt ${TO} ]; then
+  echo "[VALID ERR] You set FROM-TO is not right."
   exit 0
 fi
 
-echo $FROM
-echo $TO
-if [ ${FROM} -gt ${TO} ]; then
-  echo "[VALID ERR] You set FROM-TO is not right."
+
+################################
+#VALIDATION2
+################################
+if [ -z "${CUST}" ]; then
+  echo "[VAILD ERR] dose not set customer.${CUST}"
   exit 0
 fi
 
@@ -107,132 +137,143 @@ fi
 ################################
 #S3 ls
 ################################
-aws s3 ls ${S3_BUCKET_ADDR}/backup/ur --recursive | cut -d"/" -f3 > ${S3_DL}/tmp-s3-ls
-cat ${S3_DL}/tmp-s3-ls | awk 'BEGIN {FS="."} {print $1,$2,sprintf("%s.%s.%s",$1,$2,$3)}' | sort > ${S3_DL}/tmp-s3-ls-from-to      
+echo "S3バケットのログ情報取得中..."
+aws s3 ls ${S3_BUCKET_ADDR}/backup/${CUST} --recursive | cut -d"/" -f3 > $tmp-s3-ls
+cat $tmp-s3-ls | awk 'BEGIN {FS="."} {print $1,$2,sprintf("%s.%s.%s",$1,$2,$3)}' | sort > $tmp-s3-ls-from-to      
 
 
 ################################
 #S3からホスト名を取得(uniq)
 ################################
+echo "アプリサーバーホスト情報取得中..."
 #HOST_NAMES=`aws s3 ls ${S3_BUCKET_ADDR}/backup/ur --recursive | cut -d"/" -f3 | cut -d"." -f2`
-HOST_NAMES=`cat ${S3_DL}/tmp-s3-ls | cut -d"/" -f3 | cut -d"." -f2`
-rm -f ${S3_DL}/tmp-s3-hosts*
+HOST_NAMES=`cat $tmp-s3-ls | cut -d"/" -f3 | cut -d"." -f2`
+rm -f $tmp-s3-hosts*
 for host in $(echo ${HOST_NAMES}); do
-  echo $host >> ${S3_DL}/tmp-s3-hosts
+  echo $host >> $tmp-s3-hosts
 done
-cat ${S3_DL}/tmp-s3-hosts | awk '!colname[$1]++{print $1}' | sort > ${S3_DL}/tmp-s3-hosts-uniq
+cat $tmp-s3-hosts | awk '!colname[$1]++{print $1}' | sort > $tmp-s3-hosts-uniq
 
 
 ################################
 #ホスト名Dirを作成
 ################################
-#cat ${S3_DL}/tmp-s3-hosts-uniq
-for host in $(cat ${S3_DL}/tmp-s3-hosts-uniq); do
-  echo $host
+echo "アプリサーバーホストDir作成中..."
+#cat $tmp-s3-hosts-uniq
+for host in $(cat $tmp-s3-hosts-uniq); do
   if [ -d "${S3_DL}/$host" ]; then
     :
   else
     mkdir ${S3_DL}/$host
+    mkdir ${S3_DL}/$host/access
+    mkdir ${S3_DL}/$host/system
+    mkdir ${S3_DL}/$host/error
   fi
 done
 
 
 #########################
 #S3からログをDL
-cat ${S3_DL}/tmp-s3-ls | 
+echo "S3から指定期間に該当するログデータをダウンロード中..."
+cat $tmp-s3-ls | 
 cut -d "." -f1 | 
-awk -v from=${FROM} -v to=${TO} 'BEGIN {FS="-"} $2>=from&&$1<from || $1>=from&&$2<=to || $1<=to&&$2>=from {print $1,$2}' | awk '!colname[$1$2]++{print $1,$2}' | sort | awk '{print(sprintf("%s-%s",$1,$2))}' > ${S3_DL}/tmp-s3-from-to
+awk -v from=${FROM} -v to=${TO} 'BEGIN {FS="-"} $2>=from&&$1<from || $1>=from&&$2<=to || $1<=to&&$2>=from {print $1,$2}' | awk '!colname[$1$2]++{print $1,$2}' | sort | awk '{print(sprintf("%s-%s",$1,$2))}' > $tmp-s3-from-to
 
+join $tmp-s3-from-to $tmp-s3-ls-from-to > $tmp-s3-dl
+cat $tmp-s3-dl 
 
-#cat ${S3_DL}/tmp-s3-ls-from-to 
-#cat ${S3_DL}/tmp-s3-from-to 
-#cat ${S3_DL}/tmp-s3-ls
-
-
-join ${S3_DL}/tmp-s3-from-to ${S3_DL}/tmp-s3-ls-from-to > ${S3_DL}/tmp-s3-dl
-cat ${S3_DL}/tmp-s3-dl 
-
-for dl in $(cat ${S3_DL}/tmp-s3-dl | awk '{print $3}'); do
+for dl in $(cat $tmp-s3-dl | awk '{print $3}'); do
   aws s3 cp s3://logs.wnc.solairo-ai.com/backup/ur/${dl} ${S3_DL}/
 done
 
 
-cat ${S3_DL}/tmp-s3-dl | while read line
+#########################
+#DLしたログを解凍した後、それぞれのホストDirに振り分ける
+echo "ダウンロードしたログデータを振り分け中..."
+cat $tmp-s3-dl | while read line
 do
-  d=`echo $line | awk '{print $2}'`
-  s=`echo $line | awk '{print $3}'`
-  mv ${S3_DL}/$s ${S3_DL}/$d/
+  d=`echo $line | awk '{print $2}'`   #ホスト名Dir
+  s=`echo $line | awk '{print $3}'`   #zipファイル
+  mv ${S3_DL}/$s ${S3_DL}/$d/         #ホスト名Dirにzipを移動
+  unzip ${S3_DL}/$d/$s -d ${S3_DL}/$d #zipを解凍
+  rm -f ${S3_DL}/$d/$s                #zipを削除
+  mkdir ${S3_DL}/$d/access/${FROM_TO}
+  mkdir ${S3_DL}/$d/system/${FROM_TO}
+  mkdir ${S3_DL}/$d/error/${FROM_TO}
+  find ${S3_DL}/$d -type f -name "*access.log" | xargs -IX mv X ${S3_DL}/$d/access/${FROM_TO}
+  find ${S3_DL}/$d -type f -name "*system.log" | xargs -IX mv X ${S3_DL}/$d/system/${FROM_TO}
+  find ${S3_DL}/$d -type f -name "*error.log" | xargs -IX mv X ${S3_DL}/$d/error/${FROM_TO}
+  #rm -rf ${S3_DL}/$d/usr
 done
 
-exit 0
 
+#########################
+#振り分けたログファイルから期間外のログを削除
+echo "不必要なログデータを削除中..."
+cat $tmp-s3-dl | while read line
+do
+  d=`echo $line | awk '{print $2}'`   #ホスト名Dir
 
+  ls ${S3_DL}/$d/access/${FROM_TO}/*  | 
+  awk 'BEGIN {FS="/"} {print $NF}'    |
+  awk -v from=${FROM} -v to=${TO} 'BEGIN {FS="_"} $1<from || $1>to {print}' | 
+  xargs -IX rm -f ${S3_DL}/$d/access/${FROM_TO}/X  
 
-#DLをunzip
+  ls ${S3_DL}/$d/system/${FROM_TO}/*  | 
+  awk 'BEGIN {FS="/"} {print $NF}'    |
+  awk -v from=${FROM} -v to=${TO} 'BEGIN {FS="_"} $1<from || $1>to {print}' | 
+  xargs -IX rm -f ${S3_DL}/$d/system/${FROM_TO}/X  
 
-#Dir階層を破ってログファイルを検索（日付期間内のファイルのみ)
+  ls ${S3_DL}/$d/error/${FROM_TO}/*   | 
+  awk 'BEGIN {FS="/"} {print $NF}'    |
+  awk -v from=${FROM} -v to=${TO} 'BEGIN {FS="_"} $1<from || $1>to {print}' | 
+  xargs -IX rm -f ${S3_DL}/$d/error/${FROM_TO}/X  
+done
 
-#検索ファイルから各host ipを取得
-
-#TMP Dirを作成
-
-#TMPの下にhosot ipのDirを作成
-
-#検索ファイルをip host ごとのDirに移動
-
-#ip hostごとに以下の処理を行う
-
-#検索ファイルをaccess, system, errorごとにファイル結合（ソートは日付順）aws
-
-
-
-########################
-#S3 Bucket Check
-echo "S3バケットの確認中..."
-S3_BUCKET_IS=0
-for s3b in $(aws s3 ls); do
-  if [ "${s3b}" = "${S3_BUCKET_NAME}" ]; then
-    S3_BUCKET_IS=1
+ 
+#########################
+#振り分けたログファイルを統合する
+echo "ログデータを統合中..."
+cat $tmp-s3-dl | while read line
+LOGS_PATH=()
+do
+  d=`echo $line | awk '{print $2}'`   #ホスト名Dir
+  
+  cat ${S3_DL}/$d/access/${FROM_TO}/* >> ${S3_DL}/$d/access/${FROM_TO}.access.log
+  if [ -z "${S3_DL}/$d/access/${FROM_TO}.access.log" ]; then
+    rm -f ${S3_DL}/$d/access/${FROM_TO}.access.log
+  else
+    :  
   fi
-done
 
-#########################
-#S3 Bucket createion
-if [ ${S3_BUCKET_IS} -ne 1 ]; then
-  echo "S3バケット作成中..."
-  aws s3 mb ${S3_BUCKET_ADDR}
-  ERR_CHK "S3 バケットの作成"
-fi
+  rm -rf ${S3_DL}/$d/access/${FROM_TO}/
 
-#########################
-#S3 Upload
-echo "S3ファイルアップロード準備中..."
-aws s3 sync ./backup ${S3_BUCKET_ADDR}/backup/${CUST}
-ERR_CHK "バックアップファイルをS3にアップロード"
-
-#########################
-#S3 Uploaded check
-UPLOADED=$(aws s3 ls ${S3_BUCKET_ADDR}/backup/ --recursive | grep ${ZIPFILE})
-if [ -n "${UPLOADED}" ]; then
-  echo "アップロード確認結果:${UPLOADED}"
-else
-  echo "Upload Error"
-fi
-
-#########################
-#Log file delete
-for filepath in $(ls -a ${LOGD} | awk '/.log/ {print $1}'); do
-  if [ "$(echo $filepath | cut -d"_" -f1)" -le "${RDATE}" ]; then
-    echo $filepath
-    LOGS=(${LOGS[@]} "$filepath")
-    LOGS_PATH=(${LOGS_PATH[@]} "${LOGD}/$filepath")
-  fi
+  cat ${S3_DL}/$d/system/${FROM_TO}/* >> ${S3_DL}/$d/system/${FROM_TO}.system.log
+  [ -z "${S3_DL}/$d/system/${FROM_TO}.system.log" ] && rm -f ${S3_DL}/$d/system/${FROM_TO}.system.log || :
+  rm -rf ${S3_DL}/$d/system/${FROM_TO}/
+  
+  cat ${S3_DL}/$d/error/${FROM_TO}/* >> ${S3_DL}/$d/error/${FROM_TO}.error.log
+  [ -z "${S3_DL}/$d/error/${FROM_TO}.error.log" ] && rm -f ${S3_DL}/$d/error/${FROM_TO}.error.log || :
+  rm -rf ${S3_DL}/$d/error/${FROM_TO}/
 done
 
 
-rm -f ${LOGS_PATH[@]}
-rm -rf ./backup
-ERR_CHK "ローテート済のログファイルを削除"
+#########################
+#CSV化
+
+
+
+
+#########################
+#$tmpファイルの清掃
+rm -f $tmp-*
+
+
+#########################
+#ERR CHECK
+#ERR_CHK "ローテート済のログファイルを削除"
+
+
 
 echo "Exec fin."
 
